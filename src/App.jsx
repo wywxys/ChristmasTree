dd69a1356b6b31a10b141d0f308d605fdd0263ac
shan <wywxys@icloud.com>
2023-12-23 12:16:47 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/Y-0VExm0oqIE2dVR/scene.splinecode" />
+  );
+}
